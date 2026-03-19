3dcd0ddfa341b0095b705f635e8f2891401c5592
EdTru <e.truchado@gmail.com>
2026-03-19 12:37:55 +0100

    Practica 5

--- Practica 5/Practica 5.py
+++ Practica 5/Practica 5.py
@@ -6,7 +6,6 @@ import pymannkendall as mk
 from scipy.stats import linregress
 
 datillos = Dataset("Data_P5.nc", "r", format="NETCDF4") #Leemos los datos del archivo de de ERA5
-
 temp, lon, lat = datillos.variables["t2m"], datillos.variables["longitude"][:], datillos.variables["latitude"][:]
 data = temp[:]
 
@@ -25,7 +24,6 @@ a_l, b_l, cosa, cosa, cosa3 = linregress(meses, ristra_datos)
 print(linregress(meses, ristra_datos))
 prediccion_lin = np.array(meses)*a_l + b_l
 
-
 plt.scatter(meses, ristra_datos, s=10, c="red",label="Datos ERAS5")
 plt.plot(meses,prediccion_theisen,c="blue", label=rf"Ajuste Thei-Sen $m=${a.round(4)}")
 
@@ -37,13 +35,10 @@ plt.xlabel("Años")
 plt.ylabel("Promedio temperatura (ºC)")
 plt.title("Promedio temperatura en el mes Julio en Madrid en los años 1940-2025")
 
-
 resultao = mk.original_test(ristra_datos)
 print(resultao)
 print(a_l, b_l)
 print("pendiente Teil-Sen"+str(a),"intercept"+str(b))
 print("pendiente Mann-Kendall"+str(resultao[-2]),"intercept"+str(resultao[-1]))
-
-
 
 plt.show()
