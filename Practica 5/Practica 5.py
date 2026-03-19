c91359a810f572290a93731d9de611ee2458c031
EdTru <e.truchado@gmail.com>
2026-03-19 12:28:28 +0100

    Practica 5

--- Practica 5/Practica 5.py
+++ Practica 5/Practica 5.py
@@ -19,18 +19,28 @@ meses = list(range(1940,2026))
 
 a, b, cosa1, cosa2 = sps.mstats.theilslopes(ristra_datos, meses)
 prediccion_theisen = np.array(meses)*a +b
-a_l, b_l, cosa, cosa, cosa = linregress(meses, ristra_datos)
+
+
+a_l, b_l, cosa, cosa, cosa3 = linregress(meses, ristra_datos)
+print(linregress(meses, ristra_datos))
 prediccion_lin = np.array(meses)*a_l + b_l
 
 
-plt.scatter(meses, ristra_datos, s=10, c="red")
-plt.plot(meses,prediccion_theisen,c="blue")
-plt.plot(meses,prediccion_lin,c="green")
+plt.scatter(meses, ristra_datos, s=10, c="red",label="Datos ERAS5")
+plt.plot(meses,prediccion_theisen,c="blue", label=rf"Ajuste Thei-Sen $m=${a.round(4)}")
+
+plt.plot(meses,prediccion_lin,c="green", label=rf"Ajuste Lineal $m=${a_l.round(4)}")
+
+plt.legend()
+
+plt.xlabel("Años")
+plt.ylabel("Promedio temperatura (ºC)")
+plt.title("Promedio temperatura en el mes Julio en Madrid en los años 1940-2025")
 
 
 resultao = mk.original_test(ristra_datos)
 print(resultao)
-
+print(a_l, b_l)
 print("pendiente Teil-Sen"+str(a),"intercept"+str(b))
 print("pendiente Mann-Kendall"+str(resultao[-2]),"intercept"+str(resultao[-1]))
 
